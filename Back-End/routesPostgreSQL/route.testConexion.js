1e5e2aaf05aefc4a5fc9cb8bc013b4f152f0ef46
lester <lestertb@estudiantec.cr>
2020-10-18 15:33:04 -0600

    End conn

--- Back-End/routesPostgreSQL/route.testConexion.js
+++ Back-End/routesPostgreSQL/route.testConexion.js
@@ -12,10 +12,12 @@ router.get('/', (req, res) => {
   conn.query('select obtenerPersonas()').then(response => {
     return response;
   }).then(val => {
+    conn.end();
     if (val.rowCount < 1) return res.status(200).json({mensaje:"No hay datos"});
     else if (val.rowCount >= 1) return res.status(200).json(val.rows);
     else return res.status(400).json({mensaje:"Indefinido"});
   }).catch(err => {
+    conn.end();
     console.error(err);
     err.status = 500;
     return next(err);
