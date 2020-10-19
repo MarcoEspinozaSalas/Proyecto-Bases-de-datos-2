78174e985b511758b2bc79d5981cb24952467b20
Allen Jimenez Gonzalez <allenjimenezgonzalez@outlook.com>
2020-10-19 14:40:58 -0600

    Finalized job

--- DB/ScripGenerator.sql
+++ DB/ScripGenerator.sql
@@ -1,16 +1,17 @@
 --USE master
 --DROP DATABASE Proyecto1Bases2;
-CREATE DATABASE Proyecto1Bases2;
+--CREATE DATABASE Proyecto1Bases2;
 GO
 USE Proyecto1Bases2;
 GO
 
-CREATE TABLE personas2 (id INT, name VARCHAR(30));
+--CREATE TABLE personas2 (id INT PRIMARY KEY, name VARCHAR(30));
 
 GO
 CREATE OR ALTER PROCEDURE generate_insert 
 	@schema VARCHAR(50),
-	@table VARCHAR (50)
+	@table VARCHAR (50),
+	@state INT
 	AS
 	BEGIN
 
@@ -61,25 +62,36 @@ CREATE OR ALTER PROCEDURE generate_insert
 		SET @sql=@sql+@parameter_with_types+')'+ @nl +' AS ' +@nl
 		SET @sql=@sql+'INSERT INTO '+@table+'('+@column_name+')'+@nl+'values'+@nl
 		SET @sql=@sql+'('+@parameter_without_types+')'
-		print @sql;
-		exec sp_executeSQL @sql;
+		PRINT @sql;
+		IF @state = 1
+			BEGIN
+				EXEC sp_executeSQL @sql;
+				SELECT @sql AS sql_code;
+			END
+		ELSE
+			BEGIN	
+				SELECT @sql AS sql_code;
+			END
+		
 	END
 GO
 
-EXEC generate_insert 'DBO','personas';
+EXEC generate_insert 'DBO','personas',0;
 
 GO
 
 CREATE OR ALTER PROCEDURE generate_select
 	@schema VARCHAR(50),
 	@table VARCHAR (50),
-	@parameters VARCHAR (100)
+	@state INT
+	--@parameters VARCHAR (100)
 	AS
 	BEGIN
 		DECLARE @params_count INT;
-		SET @params_count = ((SELECT COUNT (value) FROM STRING_SPLIT(@parameters, ',')));
+		--SET @params_count = ((SELECT COUNT (value) FROM STRING_SPLIT(@parameters, ',')));
 		DECLARE @colums_count INT;
 		SET @colums_count = (SELECT COUNT(COLUMN_NAME) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = @table);
+		SET @params_count = @colums_count;
 
 		IF (  @params_count = @colums_count )
 			BEGIN
@@ -147,7 +159,7 @@ CREATE OR ALTER PROCEDURE generate_select
 
 						IF (@counter < @params_count-1)
 							BEGIN
-								SET @sql = @sql + ' ' + @parameter + ' = ' + @value + ' AND ';
+								SET @sql = @sql + ' ' + @parameter + ' = ' + @value + ' OR ';
 							END
 						ELSE
 							BEGIN
@@ -155,9 +167,16 @@ CREATE OR ALTER PROCEDURE generate_select
 							END			
 						SET @counter = @counter + 1;
 					END
-
 				PRINT @sql;
-				EXEC sp_executeSQL @sql;
+				IF @state = 1
+					BEGIN
+						EXEC sp_executeSQL @sql;
+						SELECT @sql AS sql_code;
+					END
+				ELSE
+					BEGIN	
+						SELECT @sql AS sql_code;
+					END
 			END
 		ELSE
 			BEGIN
@@ -165,16 +184,228 @@ CREATE OR ALTER PROCEDURE generate_select
 			END
 	END
 GO
-EXEC generate_select 'DBO','personas','2,allen';
+EXEC generate_select 'DBO','personas',0;
+GO
+
 GO
 CREATE OR ALTER PROCEDURE generate_update
+	@schema VARCHAR(50),
+	@table VARCHAR (50),
+	@filter VARCHAR(50),
+	@state INT
 	AS
+	BEGIN
+		DECLARE @params_count INT;
+		--SET @params_count = ((SELECT COUNT (value) FROM STRING_SPLIT(@parameters, ',')));
+		DECLARE @colums_count INT;
+		SET @colums_count = (SELECT COUNT(COLUMN_NAME) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = @table);
+		SET @params_count = @colums_count;
 
+		IF (  @params_count = @colums_count )
+			BEGIN
+				DECLARE 
+				@sql nvarchar(500),
+				@procedureName VARCHAR(100),
+				@type VARCHAR(100),
+				@cp int,
+				@np int,
+				@dp int,
+				@nl varchar(2),
+				@parameter_without_types VARCHAR(200),
+				@parameter_with_types VARCHAR(200),
+				@column_name VARCHAR(200)
 
+				SET @parameter_with_types=''
+				SET @parameter_without_types=''
+				SET @column_name=''
 
+				DECLARE cursor_columns CURSOR FOR
+				SELECT  COLUMN_NAME name,
+						DATA_TYPE type,
+						CHARACTER_MAXIMUM_LENGTH cp,
+						NUMERIC_PRECISION np,
+						DATETIME_PRECISION dp
+				FROM INFORMATION_SCHEMA.COLUMNS
+				WHERE TABLE_NAME=@table
 
+				SET @sql='CREATE OR ALTER PROCEDURE '+@schema+'.update_'+@table+'('
 
+				OPEN cursor_columns
+				FETCH NEXT FROM cursor_columns 
+				INTO @procedureName,@type,@cp,@np,@dp
+				WHILE @@FETCH_STATUS=0
 
+					BEGIN
+						SET @parameter_with_types=@parameter_with_types+'@'+@procedureName+ ' '+@type +', '
+						SET @parameter_without_types=@parameter_without_types+'@'+@procedureName+ ', '
+						SET @column_name=@column_name+@procedureName+ ', '
+						FETCH NEXT FROM cursor_columns 
+						INTO    @procedureName,@type,@cp,@np,@dp
+					END
+				CLOSE cursor_columns
 
+				DEALLOCATE cursor_columns
+				SET @parameter_with_types=SUBSTRING(@parameter_with_types,1,LEN(@parameter_with_types)-1)
+				SET @parameter_without_types=SUBSTRING(@parameter_without_types,1,LEN(@parameter_without_types)-1)
+				SET @column_name=SUBSTRING(@column_name,1,LEN(@column_name)-1)
+				SET @nl=CHAR(13) + CHAR(10)
+				SET @sql=@sql+@parameter_with_types+')'+ @nl +' AS ' +@nl
 
+				SET @sql=@sql+'UPDATE '+@table+' SET ';
+				
+				DECLARE @counter INT = 0;
+	
+				DECLARE @parameter VARCHAR(100);
+				DECLARE @value VARCHAR (100);
 
+				DECLARE @firstParameter VARCHAR(100);
+				DECLARE @firstValue VARCHAR (100);
+									
+				WHILE @counter < @params_count	
+					BEGIN				
+						SET @value = (SELECT value FROM STRING_SPLIT (@parameter_without_types,',') ORDER BY value DESC OFFSET @counter ROWS FETCH FIRST 1 ROWS ONLY );
+						SET @parameter = (SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = @table ORDER BY TABLE_NAME OFFSET @counter ROWS FETCH FIRST 1 ROWS ONLY ) ;
+
+						IF (@counter < @params_count-1)
+							BEGIN
+								SET @sql = @sql + ' ' + @parameter + ' = ' + @value + ' , ';
+							END
+						ELSE
+							BEGIN
+								SET @sql = @sql + ' ' + @parameter + ' = ' + @value+' ';
+							END			
+						SET @counter = @counter + 1;
+					END
+
+				SET @sql=@sql+'WHERE '+ @filter +  ' = @' + @filter + ';'
+	
+				PRINT @sql;
+				IF @state = 1
+					BEGIN
+						EXEC sp_executeSQL @sql;
+						SELECT @sql AS sql_code;
+					END
+				ELSE
+					BEGIN	
+						SELECT @sql AS sql_code;
+					END
+			END
+		ELSE
+			BEGIN
+				SELECT 0 AS output_code;
+			END
+	END
+GO
+
+EXEC generate_update  'DBO','personas','id',1;
+
+GO
+
+CREATE OR ALTER PROCEDURE generate_delete
+	@schema VARCHAR(50),
+	@table VARCHAR (50),
+	@filter VARCHAR (50),
+	@state INT
+	AS
+	BEGIN
+		DECLARE @params_count INT;
+		--SET @params_count = ((SELECT COUNT (value) FROM STRING_SPLIT(@parameters, ',')));
+		DECLARE @colums_count INT;
+		SET @colums_count = (SELECT COUNT(COLUMN_NAME) FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = @table);
+		SET @params_count = @colums_count;
+
+		IF (  @params_count = @colums_count )
+			BEGIN
+				DECLARE 
+				@sql nvarchar(500),
+				@procedureName VARCHAR(100),
+				@type VARCHAR(100),
+				@cp int,
+				@np int,
+				@dp int,
+				@nl varchar(2),
+				@parameter_without_types VARCHAR(200),
+				@parameter_with_types VARCHAR(200),
+				@column_name VARCHAR(200)
+
+				SET @parameter_with_types=''
+				SET @parameter_without_types=''
+				SET @column_name=''
+
+				DECLARE cursor_columns CURSOR FOR
+				SELECT  COLUMN_NAME name,
+						DATA_TYPE type,
+						CHARACTER_MAXIMUM_LENGTH cp,
+						NUMERIC_PRECISION np,
+						DATETIME_PRECISION dp
+				FROM INFORMATION_SCHEMA.COLUMNS
+				WHERE TABLE_NAME=@table
+
+				SET @sql='CREATE OR ALTER PROCEDURE '+@schema+'.delete_'+@table+'('
+
+				OPEN cursor_columns
+				FETCH NEXT FROM cursor_columns 
+				INTO @procedureName,@type,@cp,@np,@dp
+				WHILE @@FETCH_STATUS=0
+
+					BEGIN
+						SET @parameter_with_types=@parameter_with_types+'@'+@procedureName+ ' '+@type +', '
+						SET @parameter_without_types=@parameter_without_types+'@'+@procedureName+ ', '
+						SET @column_name=@column_name+@procedureName+ ', '
+						FETCH NEXT FROM cursor_columns 
+						INTO    @procedureName,@type,@cp,@np,@dp
+					END
+				CLOSE cursor_columns
+
+				DEALLOCATE cursor_columns
+				SET @parameter_with_types=SUBSTRING(@parameter_with_types,1,LEN(@parameter_with_types)-1)
+				SET @parameter_without_types=SUBSTRING(@parameter_without_types,1,LEN(@parameter_without_types)-1)
+				SET @column_name=SUBSTRING(@column_name,1,LEN(@column_name)-1)
+				SET @nl=CHAR(13) + CHAR(10)
+				SET @sql=@sql+@parameter_with_types+')'+ @nl +' AS ' +@nl
+
+				SET @sql=@sql+'DELETE FROM '+@table+' WHERE '+@filter+ ' = @' + @filter + ';'  ;
+				
+				
+	
+				PRINT @sql;
+				IF @state = 1
+					BEGIN
+						EXEC sp_executeSQL @sql;
+						SELECT @sql AS sql_code;
+					END
+				ELSE
+					BEGIN	
+						SELECT @sql AS sql_code;
+					END
+			END
+		ELSE
+			BEGIN
+				SELECT 0 AS output_code;
+			END
+	END
+GO
+EXEC generate_delete  'DBO','personas','id',1;
+GO
+CREATE OR ALTER PROCEDURE get_tables_schema
+	@schema VARCHAR (50)
+	AS
+	BEGIN
+		SELECT TABLE_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = @schema;
+	END
+GO
+CREATE OR ALTER PROCEDURE get_schemas
+	AS
+	BEGIN
+		SELECT DISTINCT TABLE_SCHEMA FROM INFORMATION_SCHEMA.COLUMNS;
+	END
+GO
+CREATE OR ALTER PROCEDURE get_table_columns
+	@table_name VARCHAR(50)
+	AS
+	BEGIN
+		SELECT COLUMN_NAME AS columns FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = @table_name;
+	END
+GO
+EXEC get_table_columns 'personas';
+	
